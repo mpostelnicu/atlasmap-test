5d2f2b0718c7ead5c630fe7e6454d8c8a22516c7
mpostelnicu <mpostelnicu@gmail.com>
2022-12-14 00:09:18 +0200

    working mapping with 3 sources

--- examples/camel3/camel-example-atlasmap-multidoc/src/main/java/io/atlasmap/examples/camel/main/Application.java
+++ examples/camel3/camel-example-atlasmap-multidoc/src/main/java/io/atlasmap/examples/camel/main/Application.java
@@ -22,26 +22,25 @@ public class Application extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
+
         from("timer:main?period=5000")
-            .to("direct:order-producer")
+              .to("direct:order-producer")
             .to("direct:contact-producer")
                 .to("direct:ip-producer")
-            .to("atlasmap:atlasmap-mapping-vs-mihai.adm")
+            .to("atlasmap:atlasmap-mapping-mihai.adm")
             .to("direct:outcome-consumer");
 
         from("direct:ip-producer")
-                //.to("http://ip.jsontest.com")
-                .setProperty("ip-schema", simple("{ \"ip\": \"1.2.3.4\"}"))
-                .log("ip-schema: [${exchangeProperty.ip-schema}]");
+                .to("http://ip.jsontest.com")
+                .setProperty("ip-c55dac4f-dbaf-4957-a9e4-855519da9498", body().convertToString());
 
         from("direct:order-producer")
-            .setProperty("order-schema", simple("resource:classpath:data/order.json"));
-           // .log("-->; Order: [${exchangeProperty.order-schema}]");
-        
+            .setProperty("order-schema-75bd4163-225a-458d-8e5b-c478e0fc37de", simple("resource:classpath:data/order.json"));
+
         from("direct:contact-producer")
-            .setProperty("contact-schema", simple("resource:classpath:data/contact.xml"));
-            //.log("-->; Contact: [${exchangeProperty.contact-schema}]");
-        
+            .setProperty("contact-schema-5e97d61f-d377-4d00-885f-3178cf824f39", simple("resource:classpath:data/contact.xml"));
+
         from("direct:outcome-consumer")
             .log("--< Outcome: [${body}]");
     }
